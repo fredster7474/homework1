bd11cb2bec5d4f7f223f43eda69cc70394192b81
Freddie Simons <freddie_simons@hotmail.co.uk>
2018-02-18 10:57:20 +0000

    adds select for +1 code

--- main.rb
+++ main.rb
@@ -6,13 +6,40 @@ def read_contacts
   return JSON.parse(contacts_json, { symbolize_names: true })
 end
 
-#1
 array = read_contacts
 
+#1
+
+=begin
 a =array.map do |hash|
   { "name": hash[:"name"], "phone": hash[:"phone"] }
 end
 puts a
+
+=end
+
+#2a
+
+=begin
+
+b = array.select {|x| x[:"phone"].include?("+1")}
+puts b
+
+=end
+
+#2a
+b = array.select {|x| x[:"email"].include?(".org")}
+puts b
+
+=begin
+{|k,v|
+  select if v.include?("w")}
+puts b
+
+#b = array.select.each {|v| v.include?("a")}
+#print b
+
+#results = sharks.select {|item| item.include?("a")}
 
 =begin
 return_keys = [ :"name", :"phone"]
